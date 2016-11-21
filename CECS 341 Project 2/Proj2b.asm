935b3d0fdab56e978408f4a8ee968f3e3ee87556
Mark Levie Mendoza <mark@Marks-Air.home>
2016-11-20 18:21:48 -0800

    \!alice finish project2

--- CECS 341 Project 2/Proj2b.asm
+++ CECS 341 Project 2/Proj2b.asm
@@ -1,19 +1,18 @@
 	.data
+	
 memory: .word
-	
-	
+
 	.text
 	
-	li $t0, 10 
-	li $t2, 2 		# i = 2
-	la $t3, memory	
+	li $t0, 10 		#
+	li $t2, 2 		#intialize $t2 with 2 like i = 2 in C
+	la $t3, memory		#store memory address into register $t3
 	
 loop:
+	sw $t2, 0($t3)		#store $t2's value into address pointed at by $t3
 	beq $t2, $t0, end 	#if t1 == 10 finish	
-	sw $t2, 0($t3)
-	addi $t2, $t2, 2
-	addi $t3, $t3, 4
-	addi $t1, $t1, 1  	# add 1 to t1
+	addi $t2, $t2, 2	#Add 2 to $t2 to go to the next number in the sequence
+	addi $t3, $t3, 4	#increment address where we are storing
 	j loop
 end:
 
